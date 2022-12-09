269ac125ecd4b50804e4a25b4f2065174674c231
Vinoshanpradeeps <89023571+Vinoshanpradeeps@users.noreply.github.com>
2022-12-09 18:03:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 song = "";
 song2 = "";
-function preload(){
-     song = loadSound("enemy.mp3");
-     song2 = loadSound("Heat Waves.mp3");
-}
-function setup{
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function setup(){
     canvas = createCanvas(600, 500);
-    canvas.center;
+    canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function preload(){
+    song = loadSound("enemy.mp3");  
+    song2 = loadSound("Heat Waves.mp3");
 }
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+function modelLoaded(){
+    console.log("Pose net is initialized.");
+}
+    function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left wrist x = " + leftWristX + "left wrist y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right wrist x = " + rightWristX + "right wrist y = " + rightWristY);
+    }
 }
